56ec7f1c8db00de873491d618f6c2f72fabeff61
yujinpark10 <55586915+yujinpark10@users.noreply.github.com>
2019-10-08 18:53:00 +0900

    Update CardList.php

--- CardList.php
+++ CardList.php
@@ -1,17 +1,19 @@
 <?php 
-    header("Content-Type: text/html; charset=UTF-8");
     $con = mysqli_connect("localhost", "yujinpark10", "qkr147147!!", "yujinpark10");
-    mysqli_query($con,'SET NAMES utf8');
+    //mysqli_query($con,'SET NAMES utf8');
+    //$result = mysqli_query($con, "SELECT * FROM card");
+    $result = mysqli_query($con,"SELECT * FROM card, member WHERE member.userID = card.userID AND card.mine = 0");
+    //$result = mysqli_query($con, "SELECT * FROM card, member WHERE member.userID = card.userID AND card.mine = 0");
 
-    $userID = $_GET["userID"];
+    $response = array(); // 배열선언
 
-    $result = mysqli_query($con, "SELECT card.name FROM card, member WHERE member.userID = card.userID AND card.mine = false");
-    $response = array();
 
     while($row = mysqli_fetch_array($result)) {
-        $array_push($response, array("cardNum"=>$row[0], "name"=>$row[1], "company"=>$row[2], "team"=>$row[3], "position"=>$row[4], "coNum"=>$row[5], "num"=>$row[6], "e_mail"=>$row[7], "faxNum"=>$row[8], "address"=>$row[9], "userID"=>$row[10], "mine"=>$row[11]));
+    	// response 라는 변수명으로 JSON 타입으로 $response 내용을 출력
+        array_push($response, array("cardNum"=>$row[0], "name"=>$row[1], "company"=>$row[2], "team"=>$row[3], "position"=>$row[4], "coNum"=>$row[5], "num"=>$row[6], "e_mail"=>$row[7], "faxNum"=>$row[8], "address"=>$row[9], "userID"=>$row[10]));
+        	
     }
 
-    echo json_encode(array("response"=>$response), JSON_UNESCAPED_UNITCODE);
-    mysql_close($con);
+    echo json_encode(array("response"=>$response));
+    mysqli_close($con);
 ?>
